b21c43f9d57ed343b35dad9b49c375d2a87ad2be
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-16 18:31:01 +0530

    added the first TypeScript file index.ts to this project

---
+++ index.ts
@@ -0,0 +1,4 @@
+function welcome(): void {
+    console.log('Welcome to this Data Structure and Algorithm with TypeScript Repository');
+    console.log('This is only for educational purpose, may not be suitable for business');
+}
