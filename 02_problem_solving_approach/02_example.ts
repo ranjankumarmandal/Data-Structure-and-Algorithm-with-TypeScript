9424b7a56021c070c5fc3aedd4756fa7cf960b61
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-27 10:52:09 +0530

    IMPORTANT - before going to code, start thinking about problem solving approach

--- 02_problem_solving_approach/02_example.ts
+++ 02_problem_solving_approach/02_example.ts
@@ -1,1 +1,10 @@
 // Problem - Count the number of letters present in a given string
+
+/*
+ Problem Solving Approach -
+ - Understand the proeblem
+ - Explore concrete examples
+ - Break it down
+ - Solve / simplify
+ - Look back and refactor
+*/
