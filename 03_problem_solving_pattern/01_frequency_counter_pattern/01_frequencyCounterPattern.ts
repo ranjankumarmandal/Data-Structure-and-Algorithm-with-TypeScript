bd01ecb0a054b2daa66247f3cea9b93c3aa5e07b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-27 17:16:44 +0530

    problem statement

--- 03_problem_solving_pattern/01_frequency_counter_pattern/01_frequencyCounterPattern.ts
+++ 03_problem_solving_pattern/01_frequency_counter_pattern/01_frequencyCounterPattern.ts
@@ -2,3 +2,6 @@
 Q. Why frequency counter pattern?
 -  It uses objects or sets to collect values / frequencies of values. I helps to avoid O(n ^ 2) and gives solution only in O(n).
 */
+
+// Problem - 2 arrays given, where 2nd containes the same value but squared with the 1st one. [hint - same frequency is going to be there]
+
