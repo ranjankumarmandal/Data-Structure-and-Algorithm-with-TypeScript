5c42672a9926eb4d90d16b3ac4e953b01087bf46
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-28 08:06:04 +0530

    added problem solving approach steps

--- 03_problem_solving_pattern/01_frequency_counter_pattern/02_anagramChallenge.ts
+++ 03_problem_solving_pattern/01_frequency_counter_pattern/02_anagramChallenge.ts
@@ -1,1 +1,11 @@
 // Problem - Write a function to determine if the given two strings are anagram of each other. Anagram means having word or phrase formed by re-arranging the letters of another.
+
+
+/*
+ Problem Solving Approach - 
+ - Understand the problem
+ - Explore concrete examples
+ - Break it down
+ - Solve / simplify
+ - Look back & refactor
+*/
