d2cc3dfee4df465dcb90fda68d76c8365c0b267e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-29 23:32:51 +0530

    added few more points

--- 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
+++ 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
@@ -1,2 +1,3 @@
 // Definition - Creting pointers or values that correspond to an index or position and move towards the begining, end or middle based on a certain condition.
+// - Very efficient for solving problems with minimal space ompleity as well
 
