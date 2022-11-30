928b21b422425caaa01a2ed89222b7edbd51de4d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-30 08:48:12 +0530

    started steps of problem solving approach

--- 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
+++ 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
@@ -2,3 +2,7 @@
 // - Very efficient for solving problems with minimal space ompleity as well
 
 // problem - write a function which accepts a sorted array of integers. It should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefindd if a pair does not exist.
+
+/*
+ Problem Solving Approach -
+*/
