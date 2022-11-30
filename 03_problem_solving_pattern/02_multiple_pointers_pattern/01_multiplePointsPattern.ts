2bc64fb385b864e1d9514ea54917ad0a984e07ef
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-30 08:37:45 +0530

    added problem statement

--- 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
+++ 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
@@ -1,3 +1,4 @@
 // Definition - Creting pointers or values that correspond to an index or position and move towards the begining, end or middle based on a certain condition.
 // - Very efficient for solving problems with minimal space ompleity as well
 
+// problem - write a function which accepts a sorted array of integers. It should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefindd if a pair does not exist.
