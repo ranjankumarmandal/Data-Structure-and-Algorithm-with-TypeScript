95c157c0207efa8e491949a483a85f4e73fb7dcd
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-11-30 09:06:35 +0530

    started working in problem solving approach - break it down

--- 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
+++ 03_problem_solving_pattern/02_multiple_pointers_pattern/01_multiplePointsPattern.ts
@@ -12,6 +12,11 @@
  - Look back & refactor
 */
 
+function sumZero(arr: number[]): number[] | undefined {
+    
+    return [];
+}
+
 sumZero([-3, -2, -1, 0, 1, 2, 3]); // output - [-3, 3]
 sumZero([-2, 0, 1, 3]); // undefined
 sumZero([1, 2, 3]); // undefined
