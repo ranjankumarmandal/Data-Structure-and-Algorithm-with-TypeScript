89531f95aa591fff29535cf0a3feefd6d5df7753
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-12-10 15:52:33 +0530

    added few more points

--- 03_problem_solving_pattern/03_sliding_window_pattern/01_slidingWindowPattern.ts
+++ 03_problem_solving_pattern/03_sliding_window_pattern/01_slidingWindowPattern.ts
@@ -1,1 +1,2 @@
 // Definition - This pattern involves creating a window which can either be an array or number from one position to another.
+// Depending on a certain condition, the window either increases or closes. Very useful for keeping track of  subset of data in an array/string etc.
