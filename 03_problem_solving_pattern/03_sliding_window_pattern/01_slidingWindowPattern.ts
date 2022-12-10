3ec465bf9bb79759752950b2f3b67d4e009d0d92
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-12-10 15:55:35 +0530

    added question

--- 03_problem_solving_pattern/03_sliding_window_pattern/01_slidingWindowPattern.ts
+++ 03_problem_solving_pattern/03_sliding_window_pattern/01_slidingWindowPattern.ts
@@ -1,2 +1,4 @@
 // Definition - This pattern involves creating a window which can either be an array or number from one position to another.
 // Depending on a certain condition, the window either increases or closes. Very useful for keeping track of  subset of data in an array/string etc.
+
+// Q. Write a function which accepts an array of integers and a number called n. This should calculate the maximum sum of n consecutive elements in the array.
