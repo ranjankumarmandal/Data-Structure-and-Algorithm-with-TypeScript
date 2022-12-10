80d30124e31d0b167c36f60ffbf89b4699b4585b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-12-10 09:43:24 +0530

    started the steps for problem solving approach

--- 03_problem_solving_pattern/02_multiple_pointers_pattern/02_countUniqueValuesChallenge.ts
+++ 03_problem_solving_pattern/02_multiple_pointers_pattern/02_countUniqueValuesChallenge.ts
@@ -1,1 +1,5 @@
 // Q. Write a function which acepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+/* 
+Problem solving approach - 
+*/
