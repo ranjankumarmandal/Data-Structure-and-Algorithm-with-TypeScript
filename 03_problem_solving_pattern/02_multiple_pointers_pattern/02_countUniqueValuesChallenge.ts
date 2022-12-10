e66b348ef15ba2670b5c88773c3f40cf64a81a2a
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-12-10 09:50:20 +0530

    started working for 3rd step of problem solving approach - Break it down

--- 03_problem_solving_pattern/02_multiple_pointers_pattern/02_countUniqueValuesChallenge.ts
+++ 03_problem_solving_pattern/02_multiple_pointers_pattern/02_countUniqueValuesChallenge.ts
@@ -9,6 +9,11 @@ Problem solving approach -
 - Look back & refactor 
 */
 
+function countUniqueValues(arr: number[]): number {
+    
+    return 2;
+}
+
 countUniqueValues([1, 1, 1, 1, 2]); // output - 2
 countUniqueValues([-1, 1, 1, 2, 3, 3, 4, 5, 6]); // 7
 countUniqueValues([]); // 0
