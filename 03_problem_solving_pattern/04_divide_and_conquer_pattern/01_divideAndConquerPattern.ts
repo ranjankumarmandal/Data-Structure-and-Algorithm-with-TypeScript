e367140d710b1a918a925513e6e25671b90533e4
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-12-12 07:58:18 +0530

    sterted working in the same problem with divide and conquer pattern using binary search where time complexity is only O(logn)

--- 03_problem_solving_pattern/04_divide_and_conquer_pattern/01_divideAndConquerPattern.ts
+++ 03_problem_solving_pattern/04_divide_and_conquer_pattern/01_divideAndConquerPattern.ts
@@ -30,6 +30,11 @@ function search(arr: number[], k: number): number {
     return -1;
 }
 
+
+// ---------- Divide and conquer pattern - Binary search - O(logn) -----------
+
+
+
 search([1, 2, 3, 4, 5, 6], 4); // output - 3
 search([1, 2, 3, 4, 5, 6], 6); // 5
 search([1, 2, 3, 4, 5, 6], 11); // -1
