bc502a577b2adddc8e78f6819efb7c165a5a471e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-12-12 07:37:43 +0530

    added question

--- 03_problem_solving_pattern/04_divide_and_conquer_pattern/01_divideAndConquerPattern.ts
+++ 03_problem_solving_pattern/04_divide_and_conquer_pattern/01_divideAndConquerPattern.ts
@@ -1,2 +1,5 @@
 // Difinition - This pattern involves dividing a data set into smaller chunks nd then repeating a process with a subset of data.
 // This pattern can tremendously decrease time complexity.
+
+// Q. Given a sorted array of integers, write a function called search that accepts  value and returns the index where the value passed to the function is located. If the values is not found, rethrn -1.
+
